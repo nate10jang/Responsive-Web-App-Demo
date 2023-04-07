a8f500db4c79025c02e7edb47b0989385c9fdc7d
Nathan Jang <seokjujang12@gmail.com>
2023-04-07 19:04:12 +0800

    navbar update

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -4,9 +4,9 @@
 	<a href="#" class="text-green-500 font-bold text-3xl">Brand</a>
 
 	<ul class="list-none flex">
-		<li><a href="#" class="text-white p-2 font-medium text-lg">Home</a></li>
-		<li><a href="#" class="text-white p-2 font-medium text-lg">About</a></li>
-		<li><a href="#" class="text-white p-2 font-medium text-lg">Services</a></li>
-		<li><a href="#" class="text-white p-2 font-medium text-lg">Contact</a></li>
+		<li><a href="#" class="text-white p-3 font-medium text-lg">Home</a></li>
+		<li><a href="#" class="text-white p-3 font-medium text-lg">About</a></li>
+		<li><a href="#" class="text-white p-3 font-medium text-lg">Services</a></li>
+		<li><a href="#" class="text-white p-3 font-medium text-lg">Contact</a></li>
 	</ul>
 </nav>
