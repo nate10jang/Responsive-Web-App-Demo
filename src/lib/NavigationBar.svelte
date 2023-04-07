5a32bb64dce608ff74a4a7716047b640a3cb53b2
Nathan Jang <seokjujang12@gmail.com>
2023-04-07 18:28:06 +0800

    Navbar updates

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,5 +1,12 @@
 <nav
-	class="flex flex-wrap sticky top-0 h-14 px-3 py-1.5 justify-between bg-slate-800"
+	class="flex flex-wrap justify-between py-3 px-6 items-center bg-slate-800 sticky top-0"
 >
-	<h1 class="text-green-500 font-bold text-3xl">Brand</h1>
+	<a href="#" class="text-green-500 font-bold text-3xl">Brand</a>
+
+	<ul class="list-none flex">
+		<li><a href="#" class="text-white p-2 font-medium text-lg">Home</a></li>
+		<li><a href="#" class="text-white p-2 font-medium text-lg">About</a></li>
+		<li><a href="#" class="text-white p-2 font-medium text-lg">Services</a></li>
+		<li><a href="#" class="text-white p-2 font-medium text-lg">Contact</a></li>
+	</ul>
 </nav>
