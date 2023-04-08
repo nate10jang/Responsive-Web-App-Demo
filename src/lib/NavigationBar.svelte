d2765c20925536e57eea6660cf29d6d476e7d150
Nathan Jang <seokjujang12@gmail.com>
2023-04-08 17:30:24 +0800

    navbar updates

--- src/lib/NavigationBar.svelte
+++ src/lib/NavigationBar.svelte
@@ -1,12 +1,14 @@
-<nav
-	class="flex flex-wrap justify-between py-3 px-6 items-center bg-slate-800 sticky top-0"
->
-	<a href="#" class="text-green-500 font-bold text-3xl">Brand</a>
-
-	<ul class="list-none flex">
-		<li><a href="#" class="text-white p-3 font-medium text-lg">Home</a></li>
-		<li><a href="#" class="text-white p-3 font-medium text-lg">About</a></li>
-		<li><a href="#" class="text-white p-3 font-medium text-lg">Services</a></li>
-		<li><a href="#" class="text-white p-3 font-medium text-lg">Contact</a></li>
-	</ul>
-</nav>
+<header class="flex bg-slate-800 sticky top-0 py-3 px-6">
+	<nav
+		class="flex flex-wrap justify-between items-center w-full"
+	>
+		<a href="#" class="text-green-500 font-bold text-3xl">Brand</a>
+	
+		<ul class="list-none flex">
+			<li><a href="#" class="text-white p-3 font-medium text-lg">Home</a></li>
+			<li><a href="#" class="text-white p-3 font-medium text-lg">About</a></li>
+			<li><a href="#" class="text-white p-3 font-medium text-lg">Services</a></li>
+			<li><a href="#" class="text-white p-3 font-medium text-lg">Contact</a></li>
+		</ul>
+	</nav>
+</header>
